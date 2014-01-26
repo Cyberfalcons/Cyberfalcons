b5a2fe2157707f8d797a3efc39adfa21fef77613
brasquinha <b.rasquinha@gmail.com>
2014-01-26 18:04:02 -0500

    Encoder debugging ideas, commenting, robot mapping
    A bunch of small cleanup changes:
    -Added a commented in change... use driveLeftE.reset() instead of driveLeftE.start() on button press. Can't tell what the behaviour would be if we start an already started encoder.
    -did some commenting and notes asking for additional commenting
    -Started some robot mapping (PWM and Digital IO "dio" channels)
    -Minor formatting of text/comments.

--- CyberFalcons 2014/src/edu/wpi/first/wpilibj/templates/CyberFalcons2014Main.java
+++ CyberFalcons 2014/src/edu/wpi/first/wpilibj/templates/CyberFalcons2014Main.java
@@ -29,6 +29,19 @@ public class CyberFalcons2014Main extends IterativeRobot {
     final double DEADZONE = 0.1;
     final int[] shotPotValue = {1, 2, 3, 4, 5};
     final int shotReadyValue = 324;
+    
+    // PWM Channel Pinout - should be in robot map
+    int pwmDriveRight = 1;
+    int pwmDriveLeft = 2;
+    
+    
+    // Digital IO Pinout - should be in robot map
+    int dioEncoderRightA = 1;
+    int dioEncoderRightB = 2;
+    int dioEncoderLeftA = 3;
+    int dioEncoderRightB = 4;
+    
+    
     // Xbox controllers
     XBoxController xboxDriver;
     // Drive motors
@@ -60,16 +73,18 @@ public class CyberFalcons2014Main extends IterativeRobot {
         xboxDriver = new XBoxController(1); // USB port 1
 
         // Drive Motors
-        talonDriveRight = new /*Talon*/ Jaguar(/*cRIO slot*/1, /*PWM channel*/ 1);
-        talonDriveLeft = new /*Talon*/ Jaguar(/*cRIO slot*/1, /*PWM channel*/ 2);
+        talonDriveRight = new /*Talon*/ Jaguar(/*cRIO slot*/1, pwmDriveRight);
+        talonDriveLeft = new /*Talon*/ Jaguar(/*cRIO slot*/1, pwmDriveLeft);
         // Gear shifter
         shifter1 = new Solenoid(/*cRIO slot*/1, /*channel*/ 7);
         shifter2 = new Solenoid(/*cRIO slot*/1, /*channel*/ 8);
         // Drive Encoders
-        driveRightE = new Encoder(/*Digital I/O channelA*/1, /*Digital I/O channelB*/ 2);
-        driveLeftE = new Encoder(/*Digital I/O channelA*/3, /*Digital I/O channelB*/ 4, true, EncodingType.k4X);
+        driveRightE = new Encoder(dioEncoderRightA, dioEncoderRightB);
+        driveLeftE = new Encoder(dioEncoderLeftA, dioEncoderLeftB, true, EncodingType.k4X); // Check EncodingType?
         driveRightE.start();
         driveLeftE.start();
+        driveRightE.reset();
+        driveLeftE.reset();
         // Sensors
 //        neckPot = new AnalogChannel(/*cRIO slot*/1, /*channel*/ 8);
 
@@ -108,13 +123,16 @@ public class CyberFalcons2014Main extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        Watchdog.getInstance().feed(); // Tell watchdog we are running
-        // activate the robot by pushing start button
+        // Tell watchdog we are running
+        Watchdog.getInstance().feed(); 
+        
+        // Activate the robot by pushing start button
         if (xboxDriver.getBtnSTART()) {
             teleopActive = true;
         }
         if (!teleopActive) {
-            return;         //only run if teleop is active
+            // Only run if teleop has been active
+            return;         
         }
 
         drive();
@@ -136,14 +154,20 @@ public class CyberFalcons2014Main extends IterativeRobot {
         drive();
         if (xboxDriver.getBtnB()) {
             driveLeftE.start();
+            /** To debug, let's try replacing with this:
+             * driveLeftE.reset();
+             * -BR
+             */
         }
+        // Printing to console to get encoder data (here for debugging Jan 25)
         System.out.println(driveLeftE.get() + "     " + driveLeftE.getDistance() + "      " 
                 + driveLeftE.getRaw() + "       " + driveLeftE.getDirection() + "       "
                 + driveLeftE.getRate());
     }
 
     /**
-     *
+     * PLEASE ADD A DESCRIPTION OF THIS: Looks like it just checks the Driver's xbox 
+     * controller and calls the apprpriate functions? -BR
      */
     public void drive() {
         if (xboxDriver.getBtnA()) {
@@ -169,7 +193,7 @@ public class CyberFalcons2014Main extends IterativeRobot {
     }
 
     /**
-     *
+     * PLEASE ADD A DESCRIPTION OF WHAT THIS DOES - and should it be within the drive functions class? -BR
      */
     public void toggleControlFlip() {
         if (controlFlipClean) {
